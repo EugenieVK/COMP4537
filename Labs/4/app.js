4ff0bf25311d96989b3bfa49dac9927dc1884a9b
Eugenie Kim <eugenie.kim@outlook.com>
2025-02-16 13:53:00 -0800

    Deploying onto digital ocean

--- Labs/4/app.js
+++ Labs/4/app.js
@@ -1,12 +1,8 @@
-//Imports
 const messages = require("./lang/en/en");
 const http = require("http");
 const url = require("url");
 
-//Reprenets a server hosting a dictionary
 class DictionaryServer {
-    //Constructs the DicitionaryServer
-    //PRE: port must be an open port at the hosting location
     constructor(port) {
         this.port = port;
         this.dictionary = [];
@@ -14,41 +10,26 @@ class DictionaryServer {
         this.requestCount = 0;
     }
 
-    //Adds a word and its definition to the dictionary
-    //Returns false if definition couldn't be added because it already exists
-    //Returns true if definition was successfully added
     addDefinition(word, def) {
-        //Filters through dictionary to check if the word already exists within it
-        const check = this.dictionary.filter((defPair) => {
-            //defPair = {word: def} json object so we filter through its keys 
-            //[0] is to grab the first key in the json object (the only key in this case)
-            //Is the key equal to our word? Yes = add it to the list check, no = don't add it
-            return Object.keys(defPair)[0] === word; 
+        const check = this.dictionary.filter((def) => {
+            return Object.keys(def)[0] === word;
         });
 
-        
         if (check.length > 0) {
-            return false; //Check filter returned something, word exist
+            return false;
         } else {
-            //create a new json object
-            //Required to do it this way as otherwise the key was always "word"
             const newDef = JSON.parse(`{"${word}" : "${def}"}`);
-            this.dictionary.push(newDef); 
-            this.lastUpdated = this.getDate(); //Update last update date to now
-            return true; //Definition has been added
+            this.dictionary.push(newDef);
+            this.lastUpdated = this.getDate();
+            return true;
         }
     }
 
-    //Gets a definition for the word from the dictionary
-    //Returns the definition if the word is defined and null if not
     getDefinition(word) {
-        //Find the definition-word pair for the word
-        const foundWords = this.dictionary.filter((defPair) => {
-            return Object.keys(defPair)[0] === word;
+        const foundWords = this.dictionary.filter((def) => {
+            return Object.keys(def)[0] === word;
         });
 
-        //If the word was found return the definition
-        //else return null
         if (foundWords.length > 0) {
             return foundWords[0][word];
         } else {
@@ -56,95 +37,79 @@ class DictionaryServer {
         }
     }
 
-    //Gets the current date in a "Febuary 8 2025" format
     getDate() {
         const date = new Date().toDateString().split(" ");
         date.shift()
         return date.join(" ");
     }
 
-    //Handles the request
     handleRequest(req, res) {
-
-        if (req.method === "POST") { //POST request handling
-            //Get the request body
+        if (req.method === "POST") {
             let body = "";
             req.on("data", chunk => {
                 body += chunk;
             });
 
-            //Handle the post once all the request body was recieved
             req.on("end", () => {
                 this.handlePost(body, res)
-                res.end(); //send response
+                
+            res.end();
             });
-        } else if (req.method === "GET") { //GET request handling
-            //Get the word from the url
+        } else if (req.method === "GET") {
             const word = url.parse(req.url, true).query.word;
-
-            //Handle the get Request
             this.handleGet(word, res);
             
             res.end();
-        } else { //Anything but a GET or POST is unimplemented
-            res.writeHead(501); //501 - unimplemented (server error)
-
-            //Response for unimplemented server
+        } else {
+            res.writeHead(501);
             const serverRes = JSON.stringify({
                     "message": messages.messages.BadRequest,
                     "requestNumber": this.requestCount
             });
-
-            //Write response
             res.write(serverRes);
             
             res.end();
         }
     }
 
-    //Handles GET requests
     handleGet(word, res){
         let serverRes = "";
-        if(word){ //Word is not null
+        if(word){
             const def = this.getDefinition(word);
-            if(def){ //Definition exists
+            if(def){
                 serverRes = JSON.stringify({
                     "definition": def,
                     "requestNumber": this.requestCount
                 });
-                res.writeHead(200); //200 - success
+                res.writeHead(200);
 
-            } else { //No definition
+            } else {
                 serverRes = JSON.stringify({
                     "message": messages.messages.WordNotFound,
                     "requestNumber": this.requestCount
                 });
-                res.writeHead(404); //404 - not found
+                res.writeHead(404);
             }
-        } else { //No word in query
+        } else {
             serverRes = JSON.stringify({
                 "message": messages.messages.BadRequest,
                 "requestNumber": this.requestCount
             });
-            res.writeHead(400); //400 - bad request
+            res.writeHead(400);
         }
 
         res.write(serverRes);
     }
 
-    //Handles POST request
     handlePost(body, res) {
-        //Parses for the new word and definition
         const jsonBody = JSON.parse(body);
         const word = jsonBody["word"];
         const def = jsonBody["definition"];
 
         let serverRes = "";
-        if (word && def) { //Both word and definition are not null
+        if (word && def) {
             const didAdd = this.addDefinition(word, def);
-            if (didAdd){ //Word was added to dictionary
-
-                //Build the new entry message
+            if (didAdd){
                 const message = messages.messages.NewEntry
                     .replace("%1", this.lastUpdated)
                     .replace("%2", `"${word}" - ${def}`)
@@ -154,67 +119,60 @@ class DictionaryServer {
                     "message": message, 
                     "requestNumber": this.requestCount,
                 });
-                res.writeHead(200); // 200 - success
-            } else { //Word could not be added cause it was already in dictionary
+                res.writeHead(200);
+            } else {
                 serverRes = JSON.stringify({
                     "message": messages.messages.DuplicateWordWarning,
                     "requestNumber": this.requestCount
                 });
-                res.writeHead(400); // 400 - bad request
+                res.writeHead(400);
             }
-        } else { //Either or both word and definition were null
+        } else {
             serverRes = JSON.stringify({
                 "message": messages.messages.BadRequest,
                 "requestNumber": this.requestCount
             });
-            res.writeHead(400); //400 - bad request
+            res.writeHead(400);
         }
 
-        res.write(serverRes); // write response
+        res.write(serverRes);
     }
 
-    //Starts the server
     startServer() {
         http.createServer((req, res) => {
 
-            //Allowing AJAX calls
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Headers' ,'GET, POST');
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-            //Handles OPTIONS pre-flight requests from CORS
             if(req.method === "OPTIONS"){
                 res.writeHead(204);
                 res.end();
                 return;
             }
 
-            //Iterate requestCount as now a request to server is being handled 
             this.requestCount++;     
-            res.setHeader('Content-Type', 'application/json'); //returning json responses from server
-            
+            res.setHeader('Content-Type', 'application/json');
             const path = url.parse(req.url).pathname;
-            if(path === "/api/definitions" || path === "/api/definitions/"){ //Correct address used for call
+            if(path === "/api/definitions" || path === "/api/definitions/"){
 
                 this.handleRequest(req, res);
-            } else { //incorrect address
+            } else {
 
                 const serverRes = JSON.stringify({
                     "message": messages.messages.PageNotFound,
                     "requestNumber": this.requestCount
                 });
-                res.writeHead(404); // 404 - not found
+                res.writeHead(404);
                 res.write(serverRes);
                 res.end();
             }
 
-        }).listen(this.port, ()=>{ 
+        }).listen(this.port, ()=>{
             console.log(`Server is running at port ${this.port}`);
-        }); // listens on the passed in port
+        });
     }
 }
 
-//make a Dictionary Server on port 8000 and start it
 const server = new DictionaryServer(8000);
 server.startServer();
-
